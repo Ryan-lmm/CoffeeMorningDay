73a533100578025f018b37626906d5db9c5fd172
Ryan-lmm <ryanlmm4@gmail.com>
2025-02-18 05:44:34 -0300

    style: adicione as media queries

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-let list = document.querySelectorAll("li.coffee");
-let counter = 0;
-for (let i = 0; i < list.length; i++) {
+let burger = document.querySelector(".burger");
+let main = document.querySelector("main.main")
 
+burger.addEventListener("click", function() {
+  let menuMob = document.querySelector(".menu-mobile")
+  if (menuMob.classList.contains('visible')) {
+    menuMob.classList.remove('visible')
+  } else {
+    menuMob.classList.add('visible')
+  }
+})
+//
+//slider
+
+
+let slideIndex = 1
+showSlides(slideIndex)
+
+function plusSlides(n) {
+  showSlides(slideIndex += n)
+}
+
+function currentSlides(n) {
+  showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+  let slides = document.querySelectorAll("ul.grid-galery-ul-mobile > li")
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"
+  }
+  slideIndex++
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block"
+  setTimeout(showSlides, 2000)
 }
